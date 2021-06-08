4c8e70f22d8a27656ab5b8eac23fe730a8cedec8
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2021-06-08 09:54:10 -0700

    I cast fireball

---
+++ dungeonbot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const {ban_patterns, token} = require("./config.json");
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user.tag}.`);
+});
+
+client.on("guildMemberAdd", member => {
+    for (pattern of ban_patterns) {
+        if (member.user.username.toLowerCase().match(new RegExp(pattern, "i"))) {
+            member.ban({reason: "Pattern ban."});
+        }
+    }
+});
+
+client.login(token);

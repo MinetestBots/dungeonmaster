0f5c3d07ae3f9403f57323f994d1c6634bf791f9
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2021-07-03 13:03:52 -0700

    Add join/leave messages

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const {ban_patterns, token} = require("./config.json");
+const config = require("./config.json");
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}.`);
@@ -9,12 +9,31 @@ client.once("ready", () => {
     client.user.setActivity("with fire.", {type: "PLAYING"});
 });
 
+function sendSystemMessage(template, member) {
+    const channel = member.guild.channels.resolve(config.system_channel);
+    if (channel && channel.type == "text") {
+        channel.send(template
+                        .replace("$ID", member.id)
+                        .replace("$NICKNAME", member.nickname)
+                        .replace("$USERNAME", member.user.username)
+                        .replace("$TAG", member.user.tag)
+        );
+    }
+}
+
 client.on("guildMemberAdd", member => {
-    for (pattern of ban_patterns) {
+    for (pattern of config.ban_patterns) {
         if (member.user.username.toLowerCase().match(new RegExp(pattern, "i"))) {
             member.ban({reason: "Pattern ban."});
+            return;
         }
     }
+
+    if (config.join_message) sendSystemMessage(config.join_message, member);
 });
 
-client.login(token);
+client.on("guildMemberRemove", member => {
+    if (config.leave_message) sendSystemMessage(config.leave_message, member);
+});
+
+client.login(config.token);

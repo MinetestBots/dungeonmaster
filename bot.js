8ae4ff734f68f1ab7836d488a11c66cf4939e232
GreenXenith <24834740+GreenXenith@users.noreply.github.com>
2021-07-03 13:26:33 -0700

    GIF when member leaves right after joining

--- bot.js
+++ bot.js
@@ -9,6 +9,8 @@ client.once("ready", () => {
     client.user.setActivity("with fire.", {type: "PLAYING"});
 });
 
+let just_joined = {};
+
 function sendSystemMessage(template, member) {
     const channel = member.guild.channels.resolve(config.system_channel);
     if (channel && channel.type == "text") {
@@ -30,10 +32,22 @@ client.on("guildMemberAdd", member => {
     }
 
     if (config.join_message) sendSystemMessage(config.join_message, member);
+
+    // Hard coding all of this because I am lazy.
+    just_joined[member.id] = true;
+    setTimeout(() => {delete just_joined[member.id]}, 60 * 1000);
 });
+
+const gifs = [
+    "https://tenor.com/view/the-simpsons-bart-simpson-whoops-go-in-and-out-gif-15009786",
+    "https://tenor.com/view/the-simpsons-homer-simpson-bush-hide-im-out-gif-17449504",
+    "https://tenor.com/view/oh-no-top-gear-jeremy-clarkson-no-one-cares-gif-18925814",
+    "https://tenor.com/view/mouse-walkout-hi-there-bye-gif-14078209",
+];
 
 client.on("guildMemberRemove", member => {
     if (config.leave_message) sendSystemMessage(config.leave_message, member);
+    if (just_joined[member.id]) sendSystemMessage(gifs[~~(Math.random() * gifs.length)], member);
 });
 
 client.login(config.token);
